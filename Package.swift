658536f540a98df561736b2d657b6f0bf0a67bd5
Aleksey Zhutenkov <zhutenkov.a.s@sberbank.ru>
2023-09-15 01:31:45 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UnityFramework",
+    platforms: [
+            .iOS(.v13)
+        ],
+    products: [
+        .library(
+            name: "UnityFramework",
+            targets: ["UnityFramework"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "UnityFramework",
+            url: "https://aang-storage.s3.eu-west-3.amazonaws.com/UnityFramework.xcframework.zip",
+            checksum: "ed2d526345b7a4311cb1507e1803f59bfd4f4a8af7183f59f9079025d95d255b")
+    ]
+)
---
+++ Tests/UnityFrameworkTests/UnityFrameworkTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import UnityFramework
+
+final class UnityFrameworkTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(UnityFramework().text, "Hello, World!")
+    }
+}
