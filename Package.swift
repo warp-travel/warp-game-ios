262be74746407f255ad4c5d4f7536ac3467aeee0
Aleksey Zhutenkov <zhutenkov.a.s@sberbank.ru>
2023-09-18 11:35:38 +0300

    Обновлен хеш на sdk

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ let package = Package(
         .binaryTarget(
             name: "UnityFramework",
             url: "https://aang-storage.s3.eu-west-3.amazonaws.com/UnityFramework.xcframework.zip",
-            checksum: "d9d17afcdee63caa00082fcd22ee98fe02ac99b6e209138d752f9b3e8ba874a2")
+            checksum: "79e23b5d398bcff9f4e3171b57a445ce452218bc23f557415695dbbb13ce6479")
     ]
 )
