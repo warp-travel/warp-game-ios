e3adb0638732b6ec78643fdbf4f2df7d6f613f9e
Aleksey Zhutenkov <zhutenkov.a.s@sberbank.ru>
2023-09-30 13:31:03 +0300

    Обновлена библиотека до 4 0 1

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UnityFramework",
-            url: "https://aang-storage.s3.eu-west-3.amazonaws.com/UnityFramework_0_4_0_final.xcframework.zip",
-            checksum: "f0f1c912e99a0e21a3e93d66b8e9d86019bf6ff55cb07e247c2cf50fa17579df")
+            url: "https://aang-storage.s3.eu-west-3.amazonaws.com/UnityFramework_0_4_1_final.xcframework.zip",
+            checksum: "b4eb6592ecae2cdb89e23c5c2290887bee071148e282780abf9155edb66c50e6")
     ]
 )
